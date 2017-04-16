6e70f7401c083fac246839cb49bbd8b0cb4726b3
R. Evan Coffey <rogerecoffey@gmail.com>
2017-04-16 17:27:47 -0500

    Pulling to test componentDidUpdate

--- app/components/Main.js
+++ app/components/Main.js
@@ -20,49 +20,57 @@ var Main = React.createClass({
 
   getInitialState: function() {
     return { home: {}, 
-            searchResults: [] };
+            searchResults: [],
+            searchCounter: 0,
+            storeCounter: 0,
+            savedCounter: 0,
+            dismissedCounter: 0 };
   }, 
 
-    //This function is passed as a prop to the landing page. It sets the home values and search results.
-    sendSearchResults: function(home, results) {
-    this.setState({
-        home: home,
-        searchResults: results});
+  //This function is passed as a prop to the landing page. It sets the home values and search results.
+  sendSearchResults: function(home, results) {
+  this.setState({
+      home: home,
+      searchResults: results});
   },
 
-
+  componentDidUpdate: function(prevProps, prevState) {
+    if (this.state.searchCounter < 10) {
+       for (var i = 0; i < this.state.searchResults.length; i++) {
+           if (this.state.searchResults[i].status === "store" ) {
+              this.state.searchResults[i].status = "search";
+              this.setState({searchCounter: (this.state.searchCounter + 1)});
+              if (this.state.searchCounter === 10) {i=this.state.searchResults.length};
+           }
+       }
+    }
+  },
 
   render: function() {
     return (
       <Router>
-    <div>
-     <Redirect to="/home"/>
-     <Switch>
+        <div>
+          <Redirect to="/home"/>
+          <Switch>
+            {/* This is the main routes setup. You can pass props in the component after the render statement.*/}
 
-   {/* This is the main routes setup. You can pass props in the component after the render statement.*/}
+            {/* Route for the landing page.*/}
+            <Route path='/home' render={(props) => (
+            <LandingPage sendResults={this.sendSearchResults} nav={this.navigateTo} {...props} />
 
- {/* Route for the landing page.*/}
-     <Route path='/home' render={(props) => (
-    <LandingPage sendResults={this.sendSearchResults} nav={this.navigateTo} {...props} />
+            )} />
 
-    )} />
-
- {/* Route for the results page. The map and selection bar will be within this component*/}
-     <Route path='/resultsPage' render={(props) => (
-    <ResultsPage results={this.state.searchResults} 
-                 home={this.state.home} {...props}
-                 sendResults={this.sendSearchResults}
-                 nav={this.navigateTo}
-                  />
-    )} />
-
-  
-       </Switch>
-   
-   
-     </div>
-       
-       </Router>
+            {/* Route for the results page. The map and selection bar will be within this component*/}
+            <Route path='/resultsPage' render={(props) => (
+            <ResultsPage results={this.state.searchResults} 
+                       home={this.state.home} {...props}
+                       sendResults={this.sendSearchResults}
+                       nav={this.navigateTo}
+                        />
+            )} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 });
