923ae2103c392a658dbeda1bc5906d7e5cfc5e26
mchogan7 <mills.c.hogan@gmail.com>
2017-04-16 00:04:27 -0500

    basic property sidebar styling

--- app/components/children/SideBarProperty.js
+++ app/components/children/SideBarProperty.js
@@ -10,11 +10,16 @@ var SideBarProperty = React.createClass({
 
   render: function() {
     return (
-     <div>
-        <div>
-        {helpers.toTitleCase(this.props.info.address)}
+     <div className='sideBarPropHolder'>
+        <div className='sideBarProp'>
+        <div className='numberHolder'>1</div>
+        <h3>{helpers.toTitleCase(this.props.info.address)}</h3>
+        <div className='propMarket'>Market: ${(this.props.info.appraised_val).toLocaleString()}</div>
+        <div className='oneThird propInfo'>Year Built: {this.props.info.yr_built}</div>
+        <div className='oneThirdish propInfo'>Sqft: {this.props.info.sqFeet}</div>
+        <div className='oneThird propInfo'>Lot: {(this.props.info.legal_acreage * .0001).toFixed(4)} acres</div>
+        <img src="./images/Arrow.svg" className='propArrow'/>
         </div>
-        <div>${this.props.info.appraised_val}</div>
      </div>
     );
   }
