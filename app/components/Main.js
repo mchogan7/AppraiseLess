f344786a8f72c40f07ea07a89c7d5053900908c8
R. Evan Coffey <rogerecoffey@gmail.com>
2017-04-18 19:47:53 -0500

    componentDidUpdate

--- app/components/Main.js
+++ app/components/Main.js
@@ -21,10 +21,10 @@ var Main = React.createClass({
   getInitialState: function() {
     return { home: {}, 
             searchResults: [],
-            searchCounter: 0,
-            storeCounter: 0,
-            savedCounter: 0,
-            dismissedCounter: 0 };
+        	store: [],
+        	search: [],
+        	saved: [],
+        	dismissed: []};
   }, 
 
   //This function is passed as a prop to the landing page. It sets the home values and search results.
@@ -35,17 +35,75 @@ var Main = React.createClass({
   },
 
   componentDidUpdate: function(prevProps, prevState) {
-    console.log(this.state.searchResults.length)
-    if (this.state.searchCounter < 10) {
-       for (var i = 0; i < this.state.searchResults.length; i++) {
-           if (this.state.searchResults[i].status === "store" ) {
-              this.state.searchResults[i].status = "search";
-              this.setState({searchCounter: (this.state.searchCounter + 1)});
-              if (this.state.searchCounter === 10) {i=this.state.searchResults.length};
-           }
+   //componentDidUpdate()
+
+   //These functions are necessary for array.filter()
+   function isStore(value) {
+     return value.status === "store";
+   };
+   function isSearch(value) {
+     return value.status === "search";
+   };
+   function isSaved(value) {
+     return value.status === "saved";
+   };
+   function isDismissed(value) {
+     return value.status === "dismissed";
+   };
+
+   //Here, we create our variables
+   //searchResults[] is merely a copy of this.state.searchResults.
+   var searchResults = this.state.searchResults;
+   //store[], search[], saved[], and dismissed[] are populated using the .filter() method.
+   var store = this.state.searchResults.filter(isStore);
+   var search = this.state.searchResults.filter(isSearch);
+   var saved = this.state.searchResults.filter(isSaved);
+   var dismissed = this.state.searchResults.filter(isDismissed);
+   //somethingChanged is marked true if a new search entry must be created
+   var somethingChanged = false;
+
+
+   //Here, we check to see if we need to fill up search[], i.e., if it contains fewer than 10 objects
+   //If it does, we take the appropriate number of "store" objects and make them "search" objects
+   //This function should work when search[] has zero objects and when it has 9
+   if (search.length < 10) {
+     somethingChanged = true;
+     var i = 10 - search.length;
+     console.log("i = " + i);
+     while (i > 0) {
+       console.log("Pop from store, push to search, swap a status");
+       //Go ahead and decrement i
+       i--;
+       //Pop from store[], push to search[]
+       var newSearchEntry = store.pop(); //Should we be using "dequeue" instead of "pop"???
+       search.push(newSearchEntry);
+       //Find the first "store" object in searchResults[], change its status to "search"
+       for (var j = 0; j < searchResults.length; j++) {
+         if (searchResults[j].status === "store") {
+           searchResults[j].status = "search";
+           j = searchResults.length;
+         }
        }
-    }
-  },
+     }
+   }
+
+   console.log("somethingChanged = " + somethingChanged);
+   console.log("searchResults, store, search, saved, and dismissed...");
+   console.log(searchResults);
+   console.log(store);
+   console.log(search);
+   console.log(saved);
+   console.log(dismissed);
+
+
+   //At this point, the goal is for all five of our arrays to be accurate
+   //Here, we set the state using our five arrays
+   if (somethingChanged) {
+     this.setState((prevState) => {
+       return {search: search, saved: saved, dismissed: dismissed, searchResults: searchResults, store: store};
+     });
+   }
+ },
 
   render: function() {
     return (
