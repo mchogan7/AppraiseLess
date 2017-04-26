8ea7dae818f7f7a6e5df8144dd8eb6f5f6708801
mchogan7 <mills.c.hogan@gmail.com>
2017-04-25 20:40:04 -0500

    responsiveness is done

--- app/components/children/ResultsSideBar.js
+++ app/components/children/ResultsSideBar.js
@@ -40,7 +40,7 @@ var ResultsSideBar = React.createClass({
   },
 
   reportNavigate(sorted){
-    if(sorted.length >= 3) {
+    if(sorted.length >= 0) {
        this.props.nav('/ReportPage')
     }
    
